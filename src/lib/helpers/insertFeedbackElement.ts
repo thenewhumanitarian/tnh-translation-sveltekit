27d4fd76e1d58d9b2a0b74c016a9716feb5b2706
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2024-07-20 10:25:10 +0200

    Feedback El

--- src/lib/helpers/insertFeedbackElement.ts
+++ src/lib/helpers/insertFeedbackElement.ts
@@ -2,15 +2,16 @@ import { JSDOM } from 'jsdom';
 
 const FEEDBACK_ELEMENT_PARAGRAPH_OFFSET = 5;
 
-export function insertFeedbackElement(html: string, translationId: string, accessId: string): string {
+export function insertFeedbackElement(html: string, translationId: string, accessId: string, targetLanguage: string): string {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
   const feedbackElementHtml = `
     <div class="feedback-element" style="margin: 3rem auto; text-align: center; background: #eee; padding: 2rem;">
-      <p>Translation Feedback Element</p>
-      <pre>Translation ID: ${translationId}</pre>
-      <pre>Access ID: ${accessId}</pre>
+      <p style="font: bold;">Translation Feedback Element</p>
+      <pre style="margin: 0;">Translation ID: ${translationId}</pre>
+      <pre style="margin: 0;">Access ID: ${accessId}</pre>
+      <pre style="margin: 0;">Target Language: ${targetLanguage}</pre>
     </div>
   `;
 
