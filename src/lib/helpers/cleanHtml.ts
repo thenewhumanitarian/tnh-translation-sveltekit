7678e28edf0257a5398a7e74b6c67f35de405860
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2024-07-22 17:06:33 +0200

    Adds cleanHTML function

--- src/lib/helpers/cleanHtml.ts
+++ src/lib/helpers/cleanHtml.ts
@@ -1,5 +1,13 @@
+// /lib/helpers/cleanHtml.ts
+
 export function cleanHtml(html: string): string {
   let cleanedHtml = html.replace(/ dir="ltr"/g, '');
   cleanedHtml = cleanedHtml.replace(/<div id="mct-script"><\/div>/g, '');
+  cleanedHtml = removeTrailingGreaterThan(cleanedHtml);
   return cleanedHtml;
+}
+
+// Helper function to clean up translated HTML content
+function removeTrailingGreaterThan(html: string): string {
+  return html.replace(/>\s*$/, '');
 }
