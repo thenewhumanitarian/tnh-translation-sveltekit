4edffa394636a35f74c957824f6c3d65e4bde747
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2024-07-22 10:14:27 +0200

    Feedback El

--- src/lib/helpers/insertFeedbackElement.ts
+++ src/lib/helpers/insertFeedbackElement.ts
@@ -2,33 +2,23 @@ import { JSDOM } from 'jsdom';
 
 const FEEDBACK_ELEMENT_PARAGRAPH_OFFSET = 5;
 
-export function insertFeedbackElement(html: string, translationId: string, accessId: string, targetLanguage: string, hasRating: boolean): string {
+export function insertFeedbackElement(html: string, translationId: string, accessId: string, targetLanguage: string): string {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
-  let feedbackElementHtml;
-
-  if (hasRating) {
-    feedbackElementHtml = `
-      <div class="feedback-element" style="margin: 3rem auto; text-align: center; background: #eee; padding: 2rem;">
-        <p style="font-weight: bold;">You have already rated this translation.</p>
+  const feedbackElementHtml = `
+    <div class="feedback-element" style="margin: 3rem auto; text-align: center; background: #eee; padding: 2rem;">
+      <p style="font-weight: bold;">How happy are you with the translation of this article so far?</p>
+      <div class="stars">
+        <span class="star" data-rating="1">★</span>
+        <span class="star" data-rating="2">★</span>
+        <span class="star" data-rating="3">★</span>
+        <span class="star" data-rating="4">★</span>
+        <span class="star" data-rating="5">★</span>
       </div>
-    `;
-  } else {
-    feedbackElementHtml = `
-      <div class="feedback-element" style="margin: 3rem auto; text-align: center; background: #eee; padding: 2rem;">
-        <p style="font-weight: bold;">How happy are you with the translation of this article so far?</p>
-        <div class="stars">
-          <span class="star" data-rating="1">★</span>
-          <span class="star" data-rating="2">★</span>
-          <span class="star" data-rating="3">★</span>
-          <span class="star" data-rating="4">★</span>
-          <span class="star" data-rating="5">★</span>
-        </div>
-        <button class="submit-rating">Submit Rating</button>
-      </div>
-    `;
-  }
+      <button class="submit-rating">Submit Rating</button>
+    </div>
+  `;
 
   // Select the main article's .field-name-body.flow by checking its parent element to ensure it's not inside .article__series-item
   const fieldNameBodyFlow = Array.from(document.querySelectorAll('.field-name-body.flow'))
